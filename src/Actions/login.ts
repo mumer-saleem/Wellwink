5eff2ae99d95270f9816794a42032a18f3010e1e
“mumer-saleem” <umer.saleem@activekeysolutions.com>
2021-11-08 19:57:36 +0500

    updated code

--- src/Actions/login.ts
+++ src/Actions/login.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 
 import api from '../Services/api'
+
 export const testing1 = createAsyncThunk(
   'counter/testing',
   async (_, thunkAPI) => {
